b92ec06e63e392b31c086aaa09871208c2960e44
doduy291 <doduy291@gmail.com>
2021-03-07 08:47:00 +0700

    Product List Reducer & Action - Redux State in HomeScreen.js

---
+++ client/src/reducers/productReducer.js
@@ -0,0 +1,14 @@
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '../constants/productConstants';
+
+export const productListReducer = (state = { allProducts: [] }, action) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, allProducts: [] };
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, allProducts: action.payload };
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,29 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import axios from 'axios';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
-  const [allProducts, setAllProducts] = useState([]);
+  // ****** WITH REACT STATE ******//
+  // const [allProducts, setAllProducts] = useState([]);
 
-  // Not create async at arrow function in useEffect
+  // // Not create async at arrow function in useEffect
+  // useEffect(() => {
+  //   const fetchAllProducts = async () => {
+  //     const { data } = await axios.get('/api/products');
+  //     setAllProducts(data);
+  //   };
+  //   fetchAllProducts();
+  // }, []);
+
+  // ****** WITH REDUX ******//
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, allProducts } = productList;
   useEffect(() => {
-    const fetchAllProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setAllProducts(data);
-    };
-    fetchAllProducts();
-  });
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Latest Product</h1>
-      <Row>
-        {allProducts.map((prd) => (
-          <Col key={prd._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={prd} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h1> Loading... </h1>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {allProducts.map((prd) => (
+            <Col key={prd._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={prd} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 };
