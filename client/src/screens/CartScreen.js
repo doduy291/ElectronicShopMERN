dd268b0b1f8096bc32c7a0e9688ea94d9e69c985
doduy291 <doduy291@gmail.com>
2021-03-09 09:29:19 +0700

    Create Redux addToCart

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
-const CartScreen = () => {
-  return <div></div>;
+const CartScreen = ({ match, history, location }) => {
+  const productId = match.params.id;
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+  return <div> Cart </div>;
 };
 
 export default CartScreen;
